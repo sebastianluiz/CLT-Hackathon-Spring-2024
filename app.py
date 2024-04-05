0d63b85b02d4476b241e85b83753db261fa1116b
Sebastian Embler <emblerproducts@gmail.com>
2024-04-05 19:51:43 -0400

    Added initial database model system

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app);
+
+class SaleSystem(db.Model): 
+    id=db.Column(db.Integer,primary_key=True)
+    menu_item=db.Column(db.String(200), nullable=False)
+    cost = db.Column(db.Float, default=0)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+#with app.app_context(): #USED TO CREATE 'test.db' FILE
+   #db.create_all()
+
+def __repr__(self):
+    return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    
